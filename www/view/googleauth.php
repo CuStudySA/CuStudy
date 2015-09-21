ace444612bcbf363a1b7c9f557253b563bcbecaf
mbalint987 <mbalint987@gmail.com>
2015-09-21 22:37:51 +0200

    Google Auth átirányítása kikapcsolva a bejelentkezési hiba javítása idején

--- www/view/googleauth.php
+++ www/view/googleauth.php
@@ -1,11 +1,13 @@
 <?php
 	if (isset($ENV['GET']['error']))
-		die(header("Location: /?errtype=remote&prov=google")); //&errdesc={$ENV['GET']['error']}
+		//die(header("Location: /?errtype=remote&prov=google")); //&errdesc={$ENV['GET']['error']}
+		var_dump($ENV['GET']['error']);
 
 	$response = ExtConnTools::Request('https://www.googleapis.com/plus/v1/people/me',ExtConnTools::GetAccessToken($ENV['GET']['code']));
 
 	if (isset($response['error']))
-		die(header("Location: /?errtype=remote&prov=google")); //&errdesc={$response['error']['message']}
+		//die(header("Location: /?errtype=remote&prov=google")); //&errdesc={$response['error']['message']}
+		var_dump($ENV['GET']['error']);
 
 	System::ExternalLogin($response['id']);
 
