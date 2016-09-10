88c36097d8bb3a6acaf93389dda5670f62994e59
SeinopSys <seinopsys@gmail.com>
2016-09-10 17:03:19 +0200

    Fix Mantis #0000048

--- www/view/landing.php
+++ www/view/landing.php
@@ -11,6 +11,30 @@
 			<p>Szoftverünk a magyar diákok igényeire szabva segíti a mindennapi felkészülést számtalan diák számára.</p>
 			<span class="btn typcn typcn-arrow-down scrolldown"></span>
 		</div>
+		<svg version="1.1" opacity="0.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+			<line id="clock-h" fill="none" stroke="#FFF" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="50" y1="50" x2="73" y2="50"></line>
+			<line id="clock-m" fill="none" stroke="#FFF" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="50" y1="50" x2="50" y2="5"></line>
+			<circle fill="none" stroke="#FFF" stroke-width="8" stroke-miterlimit="10" cx="50" cy="50" r="45"></circle>
+		</svg>
+		<script src="/resources/js/min/web-animations.js"></script>
+		<script>
+			var hours = document.getElementById("clock-h"),
+				mins = document.getElementById("clock-m");
+			hours.animate(
+				[
+					{transform: 'rotate(0deg)'},
+					{transform: 'rotate(360deg)'}
+				],
+				{ duration: 8400, iterations: Infinity }
+			);
+			mins.animate(
+				[
+					{transform: 'rotate(0deg)'},
+					{transform: 'rotate(360deg)'}
+				],
+				{ duration: 700, iterations: Infinity }
+			);
+		</script>
 	</div>
 </div>
 <div id="main-content">
