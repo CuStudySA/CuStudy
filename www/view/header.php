cd5ab9925cd8b6b6f8a17343cc3f70edbe896173
SeinopSys <seinopsys@gmail.com>
2016-12-31 17:51:12 +0100

    Favicon & manifest fájlok a Chrome kezdőképernyőre pinnelés feature-höz

--- www/view/header.php
+++ www/view/header.php
@@ -3,7 +3,18 @@
 <head>
 <meta charset="UTF-8">
 <title><?=!empty($pages[$do]['title'])?$pages[$do]['title'].' - ':''?>CuStudy</title>
-<link rel="shortcut icon" href="/favicon.ico">
+
+<link rel="apple-touch-icon" sizes="180x180" href="/resources/img/favicons-v1/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="/resources/img/favicons-v1/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/resources/img/favicons-v1/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/resources/img/favicons-v1/manifest.json">
+<link rel="mask-icon" href="/resources/img/favicons-v1/safari-pinned-tab.svg" color="#171d2d">
+<link rel="shortcut icon" href="/resources/img/favicons-v1/favicon.ico">
+<meta name="apple-mobile-web-app-title" content="CuStudy">
+<meta name="application-name" content="CuStudy">
+<meta name="msapplication-config" content="/resources/img/favicons-v1/browserconfig.xml">
+<meta name="theme-color" content="#171d2d">
+
 <meta property="og:type" content="website" />
 <meta property="og:image" content="<?=ABSPATH?>/resources/img/logo.png">
 <meta property="og:title" content="CuStudy">
@@ -11,13 +22,6 @@
 <meta property="og:description" content="A CuStudy Software Alliance által tanulók számára fejlesztett webes alkalmazás">
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no">
 <?php
-	if (defined('DEFAULT_THEME_COLOR'))
-		$color = DEFAULT_THEME_COLOR;
-	if (!empty($pages[$do]['theme-color']))
-		$color = $pages[$do]['theme-color'];
-
-	if (!empty($color))
-		print '<meta name="theme-color" content="'.$color.'">';
 
 	foreach ($css_list as $value)
 		echo "<link rel='stylesheet' href='$value'>\n";
